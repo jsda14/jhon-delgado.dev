f2294d390ff0ab1c9888ba32d65c9d8b49a76fe6
jhonenovate <166523215+jhonenovate@users.noreply.github.com>
2026-08-06 22:58:03 -0500

    feat(update)
    - Create Docker File
    - Add allow_origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 # Configuración de CORS para permitir solicitudes del frontend web en desarrollo (Vite)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://jhon-delgado.dev"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
